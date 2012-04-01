3597e35e3077a8f8dfa07fbf0f0209a3284a3e53
Topher Brown <topher200@gmail.com>
2012-03-31 22:28:39 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fantasy::Application.config.secret_token = '94891c7014b7ad6eb9c3a818e92b827549e1b2756c822feaed3367b334690471fb515857120d098347eb412593e7764c4767e093c0a6b73f6f99c749edefec56'
