be4c9550326fce865eed74a26a5ec63978b3de7a
Topher Brown <topher200@gmail.com>
2012-04-02 01:43:26 -0400

    models: relationships from fantasy_player to stats

--- app/models/fantasy_player.rb
+++ app/models/fantasy_player.rb
@@ -1,4 +1,5 @@
 class FantasyPlayer < ActiveRecord::Base
   belongs_to :owner
   belongs_to :player
+  has_many :stats, :through => :player
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,5 @@
 class Player < ActiveRecord::Base
   belongs_to :ultimate_team
   has_many :fantasy_players
+  has_many :stats
 end
