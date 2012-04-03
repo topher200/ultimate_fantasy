73e662ac7eab3c64b4137718db4ab4807ea3181b
Topher Brown <topher200@gmail.com>
2012-04-03 01:01:36 -0400

    adding blank views/draft/index, with route

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Fantasy::Application.routes.draw do
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
+  match 'draft' => 'draft#index'
 
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
