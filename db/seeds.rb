655a502f27eb51c382a24535820e75cfe0bef742
Topher Brown <topher200@gmail.com>
2012-04-03 19:14:02 -0400

    using csv data in db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'csv'
+
+
+def find_team (team_string)
+  UltimateTeam.all.each do |team|
+    if team.name == team_string
+      return team
+    end
+  end
+
+  # We haven't found his team... we must need to make it!
+  puts 'creating team ' + team_string
+  return UltimateTeam.create(:name => team_string)
+end
+
+CSV.foreach(Rails.root.join('lib', 'data', 'players.csv'),
+            :headers => :first_row, :col_sep => "|") do |player|
+  Player.create(:name => player['name'], :number => player['number'],
+                :school => player['school'],
+                :ultimate_team => find_team(player['team']))
+end
