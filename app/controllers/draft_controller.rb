52fcf310322e9d46b64b5675dc998675ca38d0cc
Topher Brown <topher200@gmail.com>
2012-04-04 01:40:41 -0400

    draft_controller: WIP with better doc strings

--- app/controllers/draft_controller.rb
+++ app/controllers/draft_controller.rb
@@ -3,14 +3,14 @@ class DraftController < ApplicationController
     drafted_player = params[:player]
     new_owner = params[:new_owner]
 
-    puts 'drafted_player #{drafted_player}'
-    puts 'new_owner #{new_owner}'
+    puts "drafted_player #{drafted_player}"
+    puts "new_owner #{new_owner}"
     if drafted_player and new_owner
       f = FantasyPlayer.create!(:player => Player.find(drafted_player),
                                 :owner => Owner.find(new_owner))
-      puts 'player #{f.player.name}'
-      flash[:notice] =
-        "#{drafted_player.name} was successfully drafted to #{new_owner.name}."
+      puts "player #{f.player.name}"
+      flash[:notice] = "#{Player.find(drafted_player).name} was successfully
+drafted to #{Owner.find(new_owner).name}."
     end
     
     @players = Player.all
