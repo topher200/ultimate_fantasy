5823c6da6370d1a867e1132381c1b77af6521cf0
Topher Brown <topher200@gmail.com>
2012-04-04 20:29:22 -0400

    draft_controller_test: checking that we get a successful response

--- test/functional/draft_controller_test.rb
+++ test/functional/draft_controller_test.rb
@@ -3,6 +3,6 @@ require 'test_helper'
 class DraftControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
-    # assert_response :success
+    assert_response :success
   end
 end
