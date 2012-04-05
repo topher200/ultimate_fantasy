eb414e62c9d6d317b10564b5419d69e431e1e27d
Topher Brown <topher200@gmail.com>
2012-04-05 01:02:39 -0400

    draft_controller_test: adding test for drafting player

--- test/functional/draft_controller_test.rb
+++ test/functional/draft_controller_test.rb
@@ -1,10 +1,23 @@
 require 'test_helper'
 
 class DraftControllerTest < ActionController::TestCase
+  setup do
+    @player = players(:one)
+    @owner = owners(:one)
+  end
+
   test "should get index" do
     get :index
     assert_response :success
 
     assert_not_nil assigns(:players)
+  end
+
+  test "should draft player" do
+    assert_difference('FantasyPlayer.count') do
+      post :draft, player: @player, new_owner: @owner
+    end
+
+    assert_redirected_to :controller => "draft", :action => "index"
   end
 end
