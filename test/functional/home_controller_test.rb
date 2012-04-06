d0f5e176097d4f93ef4612a94fc78fb249e145b1
Topher Brown <topher200@gmail.com>
2012-04-06 01:59:41 -0400

    home_controller_test: reflecting our redirection to players

--- test/functional/home_controller_test.rb
+++ test/functional/home_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class HomeControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
-    assert_response :success
+    assert_redirected_to :controller => "players", :action => "index"
   end
 
 end
