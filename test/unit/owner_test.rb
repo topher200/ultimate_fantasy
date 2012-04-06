61bf93786f615eb3a3ef2fdd457db08baeab5e82
Topher Brown <topher200@gmail.com>
2012-04-06 19:30:29 -0400

    owner_test: make sure we get our known player in owner/player list

--- test/unit/owner_test.rb
+++ test/unit/owner_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class OwnerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @owner = owners(:one)
+    @player = players(:one)
+  end
+
+  test "known owner/player combo is in players_by_owner" do
+    assert Owner.players_by_owner[@owner].include? @player
+  end
 end
