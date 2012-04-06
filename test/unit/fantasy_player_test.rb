aa993eecadae9863d70a589cf11970cd67a20f5e
Topher Brown <topher200@gmail.com>
2012-04-06 02:48:35 -0400

    fantasy_player_test: using a known current_fantasy_player

--- test/unit/fantasy_player_test.rb
+++ test/unit/fantasy_player_test.rb
@@ -1,9 +1,17 @@
 require 'test_helper'
 
 class FantasyPlayerTest < ActiveSupport::TestCase
+  def setup
+    @drafted_player = fantasy_players(:current_fantasy_player)
+  end
+  
   test "only current players returned" do
     FantasyPlayer.current_players.each do | player |
       assert player.week == nil
     end
+  end
+
+  test "our 1 current player is returned in current player list" do
+    assert_equal @drafted_player, FantasyPlayer.current_players[0]
   end
 end
