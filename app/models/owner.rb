a73ea7b45155241615626b0af95e983a3e37d864
Topher Brown <topher200@gmail.com>
2012-04-06 19:29:42 -0400

    owner: adding a list of owners to the page

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,3 +1,13 @@
 class Owner < ActiveRecord::Base
   has_many :fantasy_players
+
+  # Returns a hash of owner => [players]
+  def self.players_by_owner
+    players_by_owner = {}
+    FantasyPlayer.current_players.each do |fantasy_player|
+      players_by_owner[fantasy_player.owner] ||= []
+      players_by_owner[fantasy_player.owner] << fantasy_player.player
+    end
+    players_by_owner
+  end
 end
