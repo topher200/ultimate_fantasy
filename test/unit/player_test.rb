397cfb75715615a0fdc1382c335e1e57f052219c
Topher Brown <topher200@gmail.com>
2012-04-06 02:50:17 -0400

    fixing up unit/player_test

--- test/unit/player_test.rb
+++ test/unit/player_test.rb
@@ -4,16 +4,22 @@ class PlayerTest < ActiveSupport::TestCase
   def setup
     @drafted_player = players(:one)
   end
-  
-  test "all drafted players are drafted" do
+
+  test "all players in drafted list are drafted" do
     Player.drafted.each do |player|
-      assert (FantasyPlayer.current_players.find(player))
+      assert (FantasyPlayer.current_players.where(:player => player))
     end
   end
-  
-  test "all undrafted players are undrafted" do
+
+  test "known drafted player is returned in the list of drafted players" do
+    assert Player.drafted.include?(@drafted_player),
+    "drafted player list #{Player.drafted} doesn't include " +
+      "known drafted player #{@drafted_player}"
+  end
+
+  test "no players in undrafted list are on a team" do
     Player.undrafted.each do |player|
-      assert (not FantasyPlayer.current_players.find_by_id(player.id))
+      assert_nil FantasyPlayer.current_players.find_by_id(player.id)
     end
   end
 end
