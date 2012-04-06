c538b437b3841e152bb93853d4ba63c6e675b316
Topher Brown <topher200@gmail.com>
2012-04-06 00:55:29 -0400

    draft: passing owner from dropdown, just need to get the player...

--- app/controllers/draft_controller.rb
+++ app/controllers/draft_controller.rb
@@ -2,6 +2,10 @@ class DraftController < ApplicationController
   def index
     puts "in draft::index"
     @players = Player.undrafted
+    @owner_array = []
+    Owner.all.each do |owner|
+      @owner_array += [[owner.name, owner.id]]
+    end
   end
 
   def draft
@@ -9,12 +13,13 @@ class DraftController < ApplicationController
     drafted_player = params[:player]
     new_owner = params[:new_owner]
 
-    puts "drafted_player #{drafted_player}"
-    puts "new_owner #{new_owner}"
+    puts "in drafted_player #{drafted_player}"
+    puts "in new_owner #{new_owner}"
+    puts "in new_owner.name #{Owner.find(new_owner).name}"
     if drafted_player and new_owner
       f = FantasyPlayer.create!(:player => Player.find(drafted_player),
                                 :owner => Owner.find(new_owner))
-      puts "player #{f.player.name}"
+      puts "in player #{f.player.name}"
       flash[:notice] = "#{Player.find(drafted_player).name} was successfully
 drafted to #{Owner.find(new_owner).name}."
     end
