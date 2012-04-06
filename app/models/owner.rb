82a6a530614bc92ccc41de27dbdc2baa745b7fdc
Topher Brown <topher200@gmail.com>
2012-04-06 19:41:20 -0400

    models/owner: making sure owner list has every owner

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -4,8 +4,10 @@ class Owner < ActiveRecord::Base
   # Returns a hash of owner => [players]
   def self.players_by_owner
     players_by_owner = {}
+    Owner.all.each do |owner|
+      players_by_owner[owner] = []
+    end
     FantasyPlayer.current_players.each do |fantasy_player|
-      players_by_owner[fantasy_player.owner] ||= []
       players_by_owner[fantasy_player.owner] << fantasy_player.player
     end
     players_by_owner
