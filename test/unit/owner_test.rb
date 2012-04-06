3ea5e0926deeabee53b2ab59e6df14e860aa9900
Topher Brown <topher200@gmail.com>
2012-04-06 19:48:07 -0400

    unit/owner_test: moar tests

--- test/unit/owner_test.rb
+++ test/unit/owner_test.rb
@@ -9,4 +9,22 @@ class OwnerTest < ActiveSupport::TestCase
   test "known owner/player combo is in players_by_owner" do
     assert Owner.players_by_owner[@owner].include? @player
   end
+
+  test "players_by_owner returns every owner" do
+    assert_equal Owner.all.sort, Owner.players_by_owner.keys.sort
+  end
+
+  test "players_by_owner returns the exact list of current players" do
+    players_from_owners = []
+    Owner.players_by_owner.values.each do |player_list|
+      player_list.each do |player|
+        players_from_owners << player
+      end
+    end
+    players_from_fantasy_list = []
+    FantasyPlayer.current_players.each do |fantasy_player|
+      players_from_fantasy_list << fantasy_player.player
+    end
+    assert_equal players_from_fantasy_list.sort, players_from_owners.sort
+  end
 end
