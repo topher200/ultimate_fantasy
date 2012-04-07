363910529232daa9fed821f9716d6812f777e356
Topher Brown <topher200@gmail.com>
2012-04-07 16:06:10 -0400

    using rails logger

--- app/controllers/draft_controller.rb
+++ app/controllers/draft_controller.rb
@@ -1,6 +1,6 @@
 class DraftController < ApplicationController
   def index
-    puts "in draft::index"
+    logger.debug "in draft::index"
     @players = Player.undrafted
     @owner_array = [["", 0]]
     Owner.all.each do |owner|
@@ -9,17 +9,13 @@ class DraftController < ApplicationController
   end
 
   def draft
-    puts "in draft::draft"
+    logger.debug "in draft::draft"
     drafted_player = params[:player]
     new_owner = params[:new_owner]
 
-    puts "in drafted_player #{drafted_player}"
-    puts "in new_owner #{new_owner}"
-    puts "in new_owner.name #{Owner.find(new_owner).name}"
     if drafted_player and new_owner
       f = FantasyPlayer.create!(:player => Player.find(drafted_player),
                                 :owner => Owner.find(new_owner))
-      puts "in player #{f.player.name}"
       flash[:notice] = "#{Player.find(drafted_player).name} was successfully
 drafted to #{Owner.find(new_owner).name}."
     end
