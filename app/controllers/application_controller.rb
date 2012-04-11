1f505efc5c6c539b7907e4434f3c634e574b2666
Topher Brown <topher200@gmail.com>
2012-04-11 02:53:00 -0400

    application_controller: confirming admin before changing anything

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  before_filter :confirm_admin, :except => [:index, :show]
+end
+
+def confirm_admin
+  if not current_user.try(:admin?)
+    logger.error("Must be Topher to do that!")
+    redirect_to :controller => '/home'
+  end
 end
