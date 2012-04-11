7e8f14fedda70514aba229ea32bac181e7158dba
Topher Brown <topher200@gmail.com>
2012-04-11 02:39:47 -0400

    updating db/schema post migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120411024321) do
+ActiveRecord::Schema.define(:version => 20120411063620) do
 
   create_table "fantasy_players", :force => true do |t|
     t.integer  "owner_id",   :null => false
@@ -77,8 +77,8 @@ ActiveRecord::Schema.define(:version => 20120411024321) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "email",                  :default => "",    :null => false
+    t.string   "encrypted_password",     :default => "",    :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -89,6 +89,7 @@ ActiveRecord::Schema.define(:version => 20120411024321) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "admin",                  :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
