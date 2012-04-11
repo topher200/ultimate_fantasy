b1b92cd79cb56a747f7f1c88834de1e62fd0d732
Topher Brown <topher200@gmail.com>
2012-04-10 22:42:39 -0400

    installing devise, and setting configuration options as per instructions

--- config/application.rb
+++ config/application.rb
@@ -44,5 +44,10 @@ module Fantasy
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # NOTE(topher): Devise installer told me to add this if I'm deploying on
+    # Heroku. Now we won't "access the database or load models while
+    # precompiling assets", because apparently that's bad.
+    config.assets.initialize_on_precompile = false
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,4 +27,7 @@ Fantasy::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+  
+  # NOTE(topher): setting by instruction on Devise install
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
