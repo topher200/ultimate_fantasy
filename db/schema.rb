2fdbe5f809316661e69053e7e5558a0bf43c874e
Topher Brown <topher200@gmail.com>
2012-04-10 22:44:04 -0400

    migrating db schema to use devise

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,11 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120402054016) do
+ActiveRecord::Schema.define(:version => 20120411024321) do
 
   create_table "fantasy_players", :force => true do |t|
-    t.integer  "owner_id", :null => false
-    t.integer  "player_id", :null => false
+    t.integer  "owner_id",   :null => false
+    t.integer  "player_id",  :null => false
     t.integer  "status"
     t.integer  "week"
     t.datetime "created_at"
@@ -31,20 +31,20 @@ ActiveRecord::Schema.define(:version => 20120402054016) do
     t.integer  "home_score"
     t.integer  "away_score"
     t.date     "date"
-    t.integer  "week", :null => false
+    t.integer  "week",         :null => false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "owners", :force => true do |t|
-    t.string   "name", :null => false
+    t.string   "name",       :null => false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "players", :force => true do |t|
     t.integer  "number"
-    t.string   "name", :null => false
+    t.string   "name",             :null => false
     t.string   "school"
     t.integer  "ultimate_team_id", :null => false
     t.integer  "height"
@@ -57,8 +57,8 @@ ActiveRecord::Schema.define(:version => 20120402054016) do
   add_index "players", ["ultimate_team_id"], :name => "index_players_on_ultimate_team_id"
 
   create_table "stats", :force => true do |t|
-    t.integer  "player_id", :null => false
-    t.integer  "game_id", :null => false
+    t.integer  "player_id",  :null => false
+    t.integer  "game_id",    :null => false
     t.integer  "goals"
     t.integer  "assists"
     t.integer  "ds"
@@ -71,9 +71,27 @@ ActiveRecord::Schema.define(:version => 20120402054016) do
   add_index "stats", ["player_id"], :name => "index_stats_on_player_id"
 
   create_table "ultimate_teams", :force => true do |t|
-    t.string   "name", :null => false
+    t.string   "name",       :null => false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "users", :force => true do |t|
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
