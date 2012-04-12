ee38508b79863f915542b06ff42980c13f31e218
Topher Brown <topher200@gmail.com>
2012-04-12 02:21:30 -0400

    populating roster page

--- app/controllers/roster_controller.rb
+++ app/controllers/roster_controller.rb
@@ -1,4 +1,8 @@
 class RosterController < ApplicationController
   def index
+    owner = User.find_owner_for_user(current_user)
+    @page_title = "#{owner.name}'s Roster"
+    @players_by_status = FantasyPlayer.current_players_for_owner_by_status(owner)
+    @statuses = [["1", "Starters"], ["2", "Negative"], [nil, "Bench"]]
   end
 end
