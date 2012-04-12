fa0e8292ff829b682468659532a63b1244967757
Topher Brown <topher200@gmail.com>
2012-04-12 02:29:02 -0400

    roster_controller_test: signing in as admin

--- test/functional/roster_controller_test.rb
+++ test/functional/roster_controller_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class RosterControllerTest < ActionController::TestCase
+  setup do
+    sign_in users(:admin)
+  end
+
   test "should get index" do
     get :index
     assert_response :success
