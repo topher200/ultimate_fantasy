f6eff7e604164f336b005d6b827a08747c318cf6
Topher Brown <topher200@gmail.com>
2012-04-12 00:42:30 -0400

    adding current_players_for_owner_by_status, for roster views

--- app/models/fantasy_player.rb
+++ app/models/fantasy_player.rb
@@ -6,4 +6,14 @@ class FantasyPlayer < ActiveRecord::Base
   def self.current_players
     self.where(:week => nil)
   end
+
+  # Returns a hash of (status => fantasy_players).
+  def self.current_players_for_owner_by_status(owner)
+    players_by_status = {}
+    ["0", "1", "2"].each do |status|
+      players_by_status[status] = self.where(:week => nil, :owner_id => owner,
+                                             :status => status.to_i)
+    end
+    players_by_status
+  end
 end
--- test/unit/fantasy_player_test.rb
+++ test/unit/fantasy_player_test.rb
@@ -3,8 +3,9 @@ require 'test_helper'
 class FantasyPlayerTest < ActiveSupport::TestCase
   def setup
     @drafted_player = fantasy_players(:current_fantasy_player)
+    @owner = owners(:one)
   end
-  
+
   test "current_players only returns players that are current" do
     FantasyPlayer.current_players.each do |player|
       assert player.week == nil
@@ -14,8 +15,20 @@ class FantasyPlayerTest < ActiveSupport::TestCase
   test "current_players includes our known current player" do
     assert FantasyPlayer.current_players.include? @drafted_player
   end
-  
+
   test "current_players returns our 4 current players" do
     assert_equal FantasyPlayer.current_players.length, 4
+  end
+
+  test "current_players_for_owner_by_status returns a hash of 3 status" do
+    players_hash = FantasyPlayer.current_players_for_owner_by_status(@owner)
+    assert_equal players_hash.keys.length, 3
+  end
+
+  test "current_players_for_owner_by_status contains our known players" do
+    players_hash = FantasyPlayer.current_players_for_owner_by_status(@owner)
+    assert players_hash["0"].include? fantasy_players(:benched_fantasy_player)
+    assert players_hash["1"].include? fantasy_players(:starting_fantasy_player)
+    assert players_hash["2"].include? fantasy_players(:negative_fantasy_player)
   end
 end
