5691719333dd94307eeda13ee59b5de331068fe3
Topher Brown <topher200@gmail.com>
2012-04-12 23:00:26 -0400

    adding error message for no Owner on User

--- app/controllers/roster_controller.rb
+++ app/controllers/roster_controller.rb
@@ -3,6 +3,9 @@ class RosterController < ApplicationController
   
   def index
     owner = User.find_owner_for_user(current_user)
+    if owner.nil?
+      puts "no Owner for that User"
+    end
     @page_title = "#{owner.name}'s Roster"
     @players_by_status = FantasyPlayer.current_players_for_owner_by_status(owner)
   end
