7841cc961bc654670962656001d491ca832e1bd9
Topher Brown <topher200@gmail.com>
2012-04-12 23:25:12 -0400

    application_controller: allowing users to set fantasy_player status

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,11 @@ class ApplicationController < ActionController::Base
 end
 
 def confirm_admin
-  if (params[:controller] != 'devise/sessions' and not current_user.try(:admin?))
+  if ((params[:controller] != 'devise/sessions') and
+      (params[:controller] != 'fantasy_players') and
+      (not current_user.try(:admin?)))
     flash[:error] = "Must be Topher to do that!"
     logger.warn("confirm_admin check failed! You must be topher to do that")
-    redirect_to :controller => '/home'
+    redirect_to :controller => 'home'
   end
 end
