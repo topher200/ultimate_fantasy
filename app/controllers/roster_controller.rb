030786560afd412b148a8b9665548e6ff2789646
Topher Brown <topher200@gmail.com>
2012-04-12 22:25:20 -0400

    completing roster view

--- app/controllers/roster_controller.rb
+++ app/controllers/roster_controller.rb
@@ -5,6 +5,5 @@ class RosterController < ApplicationController
     owner = User.find_owner_for_user(current_user)
     @page_title = "#{owner.name}'s Roster"
     @players_by_status = FantasyPlayer.current_players_for_owner_by_status(owner)
-    @statuses = [["1", "Starters"], ["2", "Negative"], [nil, "Bench"]]
   end
 end
