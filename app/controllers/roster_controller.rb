c0b93a2092b16f904da9fc0d1af032f861ba116d
Topher Brown <topher200@gmail.com>
2012-04-13 19:33:36 -0400

    roster_controller: adding flash of "set rosters"

--- app/controllers/roster_controller.rb
+++ app/controllers/roster_controller.rb
@@ -2,6 +2,7 @@ class RosterController < ApplicationController
   prepend_before_filter :authenticate_user!
   
   def index
+    flash[:notice] = "Week 1 games have started - any roster changes now will take effect next week!"
     owner = User.find_owner_for_user(current_user)
     if owner.nil?
       puts "no Owner for that User"
